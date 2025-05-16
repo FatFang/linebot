ea00e34e7dc4f7bc7a3beacd64c2922f86217e34
FatFang <102510557+FatFang@users.noreply.github.com>
2025-05-16 15:05:08 +0800

    Update linebot_test2.py

--- linebot_test2.py
+++ linebot_test2.py
@@ -135,3 +135,7 @@ def handle_message(event):
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
+
+
+
+#https://1cb4-122-116-207-128.ngrok-free.app/api/history
